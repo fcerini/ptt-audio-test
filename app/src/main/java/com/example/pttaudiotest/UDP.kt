5b141f6d88e5babab03c7f09aa35a6f0a01e2b7b
Fernando Cerini <fcerini@gmail.com>
2023-07-21 12:44:42 -0300

    UDP + decode

---
+++ app/src/main/java/com/example/pttaudiotest/UDP.kt
@@ -0,0 +1,100 @@
+package com.example.pttaudiotest
+
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
+import com.theeasiestway.opus.Constants
+import com.theeasiestway.opus.Opus
+
+class UDP {
+    var audios = emptyArray<ByteArray>()
+
+    private val remoteHost = "190.2.45.173"//""172.31.120.230"//"127.0.0.1"
+    private val remotePort = 64749 //64739
+
+    private val codec = Opus()
+    private var running = false
+    private val buffer = ByteArray(2048)
+    private var socket: DatagramSocket? = null
+
+    fun init() {
+        socket = DatagramSocket()
+        socket!!.broadcast = true
+        running = true
+    }
+
+    fun ping() {
+        Thread(Runnable {
+            Thread.sleep(500)
+            pingLoop()
+        }).start()
+    }
+
+    fun receive() {
+        Thread(Runnable {
+            Thread.sleep(500)
+            receiveLoop()
+        }).start()
+    }
+
+    private fun pingLoop() {
+        while (running) {
+            try {
+                val ping = byteArrayOf(99.toByte(), 0.toByte(), 0.toByte(), 0.toByte())
+                val sendPacket =
+                    DatagramPacket(ping, ping.size, InetAddress.getByName(remoteHost), remotePort)
+                socket?.send(sendPacket)
+                println("UDP PING... ")
+                Thread.sleep(1000 * 10)
+
+            } catch (e: Exception) {
+                println("UDP ERR PING " + e.message)
+                Thread.sleep(100)
+            }
+
+        }
+    }
+
+    private fun receiveLoop() {
+        while (running) {
+            try {
+                val packet = DatagramPacket(buffer, buffer.size)
+
+                socket?.receive(packet)
+
+                if (packet.length < 10) {
+                    println("UDP ping? " + packet.data.slice(0 until packet.length).toByteArray().contentToString())
+                    continue
+                }
+
+                val headerLen = packet.length - 40
+                val header = packet.data.slice(0 until headerLen).toByteArray()
+                val payload = packet.data.slice(headerLen until packet.length).toByteArray()
+
+                println("UDP " + header.contentToString() + ":" + payload.contentToString())
+
+                val decoded =
+                    codec.decode(
+                        bytes = payload,
+                        frameSize = Constants.FrameSize._1920()
+                    )
+
+                if (decoded != null) {
+                    audios += decoded
+                } else {
+                    println("UDP ERR decoded = null!!")
+                }
+
+            } catch (e: Exception) {
+                println("UDP ERR " + e.message)
+                Thread.sleep(100)
+            }
+        }
+    }
+
+    fun end() {
+        running = false
+        socket?.close()
+    }
+}
